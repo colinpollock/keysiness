2e570b9d4f7b93f928ad5a3218b611f64a6cc879
Colin <colin@wordnik.com>
2012-02-22 20:32:15 -0800

    First commit: some sample data and a script to gather more data.

---
+++ getdata.py
@@ -0,0 +1,83 @@
+"""Command line interface for timing how long it takes a user to type words. """
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+import random
+import sys
+from time import time
+
+
+DATA_FILENAME = 'data.json'
+
+
+def load_data():
+    """Return a (list, dict) pair. The list is words yet to be done and the
+    dict maps from completed words to time taken to do them.
+    """
+
+    try:
+        fh = open(DATA_FILENAME, 'r')
+    except IOError:
+        print >> sys.stderr, '* * * File "data.json" must already exist * * *'
+        raise
+
+    else:
+        return tuple(json.load(fh))
+
+
+def store_data(incomplete, completed):
+    """Store a pair (incomplete, completed) in `DATA_FILENAME`."""
+
+    with open(DATA_FILENAME, 'w') as out:
+        json.dump([incomplete, completed], out, indent=3)
+    
+
+
+def time_word_typing(word):
+    """Return the time it takes for the user to type the word in. Return None
+    if the user misspells the word.
+    """
+
+    #TODO: maybe give user a few ms to read word and then request them to type
+    t0 = time()
+    typed = raw_input(word + '\n')
+    if typed.strip() == word:
+        return time() - t0
+    else:
+        return None
+
+
+
+def main(args):
+    incomplete, completed = load_data()
+    random.shuffle(incomplete)
+    word = None
+    try:
+        while incomplete:
+            word = incomplete.pop() 
+
+            time_taken = time_word_typing(word)
+            if time_taken is None:
+                incomplete.append(word)
+                random.shuffle(incomplete)
+            else:
+                completed[word] = time_taken
+
+            word = None
+            
+
+    except EOFError:
+        if word is not None:
+            incomplete.append(word)
+    
+    finally:
+        store_data(incomplete, completed)
+        print
+        return
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
